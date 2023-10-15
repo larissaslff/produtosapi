23c56222a6dab20915fc9c8242e920f8d9354a4f
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-15 10:50:04 -0300

    add listar method on service

--- src/main/java/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/com/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,18 @@
 package com.api.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.produtos.modelo.Produto;
+import com.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
     
+    @Autowired
+    private ProdutoRepositorio repositorio;
+
+    public Iterable<Produto> listar(){
+        return repositorio.findAll();
+    }
 }
