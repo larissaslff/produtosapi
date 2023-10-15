5cb165b8aa53ed2274ab1b0b1b9a74cbeb633f46
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-15 11:23:29 -0300

    fix cross

--- src/main/java/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/com/api/produtos/controle/ProdutoControle.java
@@ -2,6 +2,7 @@ package com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.api.produtos.modelo.Produto;
 import com.api.produtos.modelo.RespostaModelo;
 import com.api.produtos.servico.ProdutoServico;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoControle {
