34b7d8fc820f5c1844daf9c87383e557aebafb01
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-10-15 11:20:38 -0300

    add remover method on productcontroller

--- src/main/java/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/com/api/produtos/controle/ProdutoControle.java
@@ -2,7 +2,9 @@ package com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.produtos.modelo.Produto;
+import com.api.produtos.modelo.RespostaModelo;
 import com.api.produtos.servico.ProdutoServico;
 
 @RestController
@@ -37,5 +40,10 @@ public class ProdutoControle {
     @PutMapping("/alterar")
     public ResponseEntity<?> alterar(@RequestBody Produto produto) {
         return servico.cadastrarAlterar(produto, "alterar");
+    }
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable("codigo") Long codigo) {
+        return servico.remover(codigo);
     }
 }
